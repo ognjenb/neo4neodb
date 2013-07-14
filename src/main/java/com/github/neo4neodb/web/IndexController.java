c680667e87c7eafb57ddf14dc6d20b2426e94445
Shahzada Hatim <hatim@hatimonline.com>
2013-07-14 18:23:50 +0200

    the ability to store atleast one entitty with spring data neo4j

--- src/main/java/com/github/neo4neodb/web/IndexController.java
+++ src/main/java/com/github/neo4neodb/web/IndexController.java
@@ -1,6 +1,9 @@
 package com.github.neo4neodb.web;
 
 
+import com.github.neo4neodb.domain.Observer;
+import com.github.neo4neodb.repository.ObserverRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @SuppressWarnings("unused")
 public class IndexController {
 
+    @Autowired
+    ObserverRepository observerRepository;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Model model) {
         model.addAttribute("date", new java.util.Date());
+        Observer observer = new Observer((new java.util.Date()).toString());
+        observerRepository.save(observer);
         return "index";
     }
 
